2f5ce99aa724f642698abe568db86cb5640f9d47
CarlosHxH <carlosdaniel.prog@gmail.com>
2025-06-10 18:27:40 -0400

    Tested

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    env: {
-        AUTH_SECRET: process.env.AUTH_SECRET,
-        DATABASE_URL: process.env.DATABASE_URL,
-    },
-    allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
-}
